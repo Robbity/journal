ceb426bdea0c8b9ded03426889d712b9fac2a152
Robbity <robbielaughlen@gmail.com>
2026-03-16 13:46:05 +0000

    mvp without styling

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root 'entries#index'
+  resources :entries, only: [:create, :new, :destroy]
 end
